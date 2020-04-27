716d954847b02b765e63cb82b485b16d496fbcf2
Benjamin <benjaminsei12@gmail.com>
2020-04-27 04:18:13 -0400

    Added fade to dusk

--- assignment_package/glsl/sky.frag.glsl
+++ assignment_package/glsl/sky.frag.glsl
@@ -1,7 +1,7 @@
 #version 150
 uniform mat4 u_ViewProj;    // We're actually passing the inverse of the viewproj
-                            // from our CPU, but it's named u_ViewProj so we don't
-                            // have to bother rewriting our ShaderProgram class
+// from our CPU, but it's named u_ViewProj so we don't
+// have to bother rewriting our ShaderProgram class
 
 uniform ivec2 u_Dimensions; // Screen dimensions
 
@@ -15,23 +15,27 @@ const float FAR_CLIP = 1000.0;
 const float PI = 3.14159265359;
 const float TWO_PI = 6.28318530718;
 
+// sky flavors
+#define SUNSET_THRESHOLD 0.75
+#define DUSK_THRESHOLD -0.1
+
 // sun parameters
 const vec4 sunDir = normalize(vec4(0.5, 1, 0.75, 0));
-const float sunSize = 30.0;
+const float sunSize = 20.0;
 const float sunCoreSize = 7.5;
 
 // Sunset palette
 const vec3 sunset[5] = vec3[](vec3(255, 229, 119) / 255.0,
-                               vec3(254, 192, 81) / 255.0,
-                               vec3(255, 137, 103) / 255.0,
-                               vec3(253, 96, 81) / 255.0,
-                               vec3(57, 32, 51) / 255.0);
+vec3(254, 192, 81) / 255.0,
+vec3(255, 137, 103) / 255.0,
+vec3(253, 96, 81) / 255.0,
+vec3(57, 32, 51) / 255.0);
 // Dusk palette
 const vec3 dusk[5] = vec3[](vec3(144, 96, 144) / 255.0,
-                            vec3(96, 72, 120) / 255.0,
-                            vec3(72, 48, 120) / 255.0,
-                            vec3(48, 24, 96) / 255.0,
-                            vec3(0, 24, 72) / 255.0);
+vec3(96, 72, 120) / 255.0,
+vec3(72, 48, 120) / 255.0,
+vec3(48, 24, 96) / 255.0,
+vec3(0, 24, 72) / 255.0);
 
 const vec3 sunColor = vec3(255, 255, 190) / 255.0;
 const vec3 cloudColor = sunset[3];
@@ -110,10 +114,15 @@ void main()
     // convert ray to 2d uv coords
     vec2 uv = sphereToUV(rayDir /*- clamp(sin(u_Eye * 0.01), 0.f, .75f)*/);
 
-    vec3 col  = toSunset(uv.y);
+    float skyInput = uv.y;
+    vec3 sunsetCol = toSunset(skyInput);
+    vec3 duskCol = toDusk(skyInput);
+
+    vec3 col = sunsetCol;
     // recall the definition of a dot product. The angle between two normalized vectors
     // can be found by taking the arccos(a dot b). theta: [0,pi]. Multiply by 2 to get [0,two_pi]
-    float angle = acos(dot(rayDir, sunDir.xyz)) * 2.f * (180.f / PI);
+    float raySunDot = dot(rayDir, sunDir.xyz);
+    float angle = acos(raySunDot) * 2.f * (180.f / PI);
 
     if (angle < sunSize) {
         if (angle < sunCoreSize) {
@@ -125,6 +134,15 @@ void main()
             float coronaDist = (angle - sunCoreSize) / (sunSize - sunCoreSize);
             coronaDist = smoothstep(0.0, 1.0, coronaDist);
             col = mix(sunColor, col, coronaDist);
+        }
+    } else {
+        if (raySunDot > DUSK_THRESHOLD) {
+            col = col;
+        } else if (raySunDot > DUSK_THRESHOLD) {
+            float t = (raySunDot - SUNSET_THRESHOLD) / (DUSK_THRESHOLD - SUNSET_THRESHOLD);
+            col = mix(col, duskCol, t);
+        } else {
+            col = duskCol;
         }
     }
 
